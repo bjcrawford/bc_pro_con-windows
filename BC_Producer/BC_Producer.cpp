fe1388b712ec0712f8d8567663ddc8223e6c0f12
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-22 15:35:47 -0400

    Added documentation to the producer class

--- BC_Producer/BC_Producer.cpp
+++ BC_Producer/BC_Producer.cpp
@@ -1,16 +1,29 @@
-/* File: BC_Producer.cpp
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Producer.cpp 
+ * @author Brett Crawford
+ * @brief A producer class
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: A producer class to be used with an implementation of the 
+ *  producer/consumer problem. The producer class is to be used in conjunction
+ *  with a shared buffer and a shared logger as well as with other producers
+ *  and consumers. This class is written for use with windows.
 */
 
 using namespace std;
 
 #include "BC_Producer.hpp"
 
+/**
+ * Constructs a producer object. 
+ * 
+ * @param[in] id An identifer for the producer
+ * @param[in] buffer A pointer to the shared buffer
+ * @param[in] logger A pointer to the shared logger
+*/
 BC_Producer::BC_Producer(int id, BC_Buffer *buffer, BC_Logger *logger)
 {
 	this->id = id;
@@ -18,11 +31,21 @@ BC_Producer::BC_Producer(int id, BC_Buffer *buffer, BC_Logger *logger)
 	this->logger = logger;
 }
 
+/**
+ * Destroys a producer object
+*/
 BC_Producer::~BC_Producer()
 {
-
+	this->buffer = NULL;
+	this->logger = NULL;
 }
 
+/**
+ * Produces a pseudo-randomly generated number between 0 and 9999,
+ * stores its value in dynamically allocated memory, inserts a 
+ * pointer to the value into the buffer, and logs the event with
+ * logger.
+*/
 void BC_Producer::produce()
 {
 	char *event = (char*) calloc(65, sizeof(char));
--- BC_Producer/BC_Producer.hpp
+++ BC_Producer/BC_Producer.hpp
@@ -1,10 +1,13 @@
-/* File: BC_Producer.h
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Producer.hpp
+ * @author Brett Crawford
+ * @brief A producer class header
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: The header for the producer class.
 */
 
 #ifndef BC_PRODUCER_H_
@@ -15,13 +18,18 @@
 
 #include "../BC_Buffer/BC_Buffer.hpp"
 #include "../BC_Logger/BC_Logger.hpp"
-
+ 
+/** 
+ * A producer class to be used with an implementation of the producer/consumer 
+ * problem. The producer class is to be used in conjunction with a shared 
+ * buffer and a shared logger as well as with other producers and consumers.
+*/
 class BC_Producer
 {
 	private:
-		int id;
-		BC_Buffer *buffer;
-		BC_Logger *logger;
+		int id;              /**< An identifier for the producer */
+		BC_Buffer *buffer;   /**< A pointer to the shared buffer */
+		BC_Logger *logger;   /**< A pointer to the shared logger */
 
 	public:
 		BC_Producer(int id, BC_Buffer*, BC_Logger*);
