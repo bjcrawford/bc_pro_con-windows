9774c112d1d7661c9ea313ae27613e3c5925bcba
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-22 15:27:30 -0400

    Added documentation to the consumer class

--- BC_Consumer/BC_Consumer.cpp
+++ BC_Consumer/BC_Consumer.cpp
@@ -1,16 +1,29 @@
-/* File: BC_Consumer.cpp
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Consumer.cpp
+ * @author Brett Crawford
+ * @brief A consumer class
+ * @details 
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: A consumer class to be used with an implementation of the 
+ *  producer/consumer problem. The consumer class is to be used in conjunction
+ *  with a shared buffer and a shared logger as well as with other producers
+ *  and consumers. This class is written for use with windows.
 */
 
 using namespace std;
 
 #include "BC_Consumer.hpp"
 
+/**
+ * Constructs a consumer object. 
+ * 
+ * @param[in] id An identifer for the consumer
+ * @param[in] buffer A pointer to the shared buffer
+ * @param[in] logger A pointer to the shared logger
+*/
 BC_Consumer::BC_Consumer(int id, BC_Buffer *buffer, BC_Logger *logger)
 {
 	this->id = id;
@@ -18,11 +31,18 @@ BC_Consumer::BC_Consumer(int id, BC_Buffer *buffer, BC_Logger *logger)
 	this->logger = logger;
 }
 
+/**
+ * Destroys a consumer object
+*/
 BC_Consumer::~BC_Consumer()
 {
 
 }
 
+/**
+ * Consumes pointer from the buffer, logs the event with the logger, and 
+ * releases the memory used by the pointer.
+*/
 void BC_Consumer::consume()
 {
 	char *event = (char*) calloc(65, sizeof(char));
--- BC_Consumer/BC_Consumer.hpp
+++ BC_Consumer/BC_Consumer.hpp
@@ -1,10 +1,13 @@
-/* File: BC_Consumer.h
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Consumer.hpp
+ * @author Brett Crawford
+ * @brief A consumer class header
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: The header for the consumer class.
 */
 
 #ifndef BC_CONSUMER_H_
@@ -13,12 +16,17 @@
 #include "../BC_Buffer/BC_Buffer.hpp"
 #include "../BC_Logger/BC_Logger.hpp"
 
+/**  
+ * A consumer class to be used with an implementation of the producer/consumer 
+ * problem. The consumer class is to be used in conjunction with a shared 
+ * buffer and a shared logger as well as with other producers and consumers.
+*/
 class BC_Consumer
 {
 	private:
-		int id;
-		BC_Buffer *buffer;
-		BC_Logger *logger;
+		int id;              /**< An identifier for the producer */
+		BC_Buffer *buffer;   /**< A pointer to the shared buffer */
+		BC_Logger *logger;   /**< A pointer to the shared logger */
 
 	public:
 		BC_Consumer(int, BC_Buffer*, BC_Logger*);
