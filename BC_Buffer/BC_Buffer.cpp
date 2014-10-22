6882b7a8ee9f34639b0fc2d3c037265aa7efe8a5
Brett Crawford <brettjamescrawford@gmail.com>
2014-10-22 15:18:23 -0400

    Added separate mutex locks for insertion and removal to and from the buffer, Added buffer documentation

--- BC_Buffer/BC_Buffer.cpp
+++ BC_Buffer/BC_Buffer.cpp
@@ -1,39 +1,63 @@
-/* File: BC_Buffer.cpp
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Buffer.cpp 
+ * @author Brett Crawford
+ * @brief A shared, bounded buffer class
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: A sharedbounded buffer class to be used with an implementation
+ *  of the producer/consumer problem. The buffer class is to be used in 
+ *  conjunction with a shared logger as well as with producers and consumers. 
+ *  This class is written for use with windows.
 */
 
 using namespace std;
 
 #include "BC_Buffer.hpp"
 
-
+/**
+ * Constructs a shared, bounded buffer object. 
+ * 
+ * @param[in] size The max number of elements in buffer
+ * @param[in] logger A pointer to the shared logger
+*/
 BC_Buffer::BC_Buffer(size_t size, BC_Logger *logger)
 {
 	first = last = 0;
 	this->size = size;
 	this->logger = logger;
 	buffer = (void**) calloc(this->size, sizeof(void*));
-	mutex_lock = CreateMutex(NULL, FALSE, NULL);
+	mutex_insert_lock = CreateMutex(NULL, FALSE, NULL);
+	mutex_remove_lock = CreateMutex(NULL, FALSE, NULL);
 	sem_available = CreateSemaphore(NULL, this->size, this->size, NULL);
 	sem_unavailable = CreateSemaphore(NULL, 0, this->size, NULL);
 }
 
+/**
+ * Destroys a buffer object
+*/
 BC_Buffer::~BC_Buffer()
 {
-	int i;
+	size_t i;
+	WaitForSingleObject(mutex_insert_lock, INFINITE);
+	WaitForSingleObject(mutex_remove_lock, INFINITE);
 	for(i = first; i < last; i++)
 		free(buffer[i % size]);
 	free(buffer);
-	CloseHandle(mutex_lock);
+	CloseHandle(mutex_insert_lock);
+	CloseHandle(mutex_remove_lock);
 	CloseHandle(sem_available);
 	CloseHandle(sem_unavailable);
 }
 
+/**
+ * Inserts an element in the buffer. If the buffer is full, this call will
+ * block until the insert can be performed. 
+ *
+ * @param[in] item A void pointer to the element to be inserted
+*/
 void BC_Buffer::insert(void *item)
 {
 	
@@ -42,21 +66,34 @@ void BC_Buffer::insert(void *item)
 	ReleaseSemaphore(sem_unavailable, 1, NULL);
 }
 
+/**
+ * For internal class use only. Inserts an element in the buffer. If this 
+ * method has been called, the buffer is guaranteed to be in a not full 
+ * state. This call will block if the shared data of the buffer is currently
+ * in use.  
+ *
+ * @param[in] item A void pointer to the element to be inserted
+*/
 void BC_Buffer::insert_internal(void *item)
 {
-	int f, l;
+	int l;
 	char *event = (char*) calloc(65, sizeof(char));
-	WaitForSingleObject(mutex_lock, INFINITE);
+	WaitForSingleObject(mutex_insert_lock, INFINITE);
 	buffer[last % size] = item;
 	last++;
-	f = first;
 	l = last;
-	ReleaseMutex(mutex_lock);
-	sprintf_s(event, 65, "Buffer: %d inserted, l - f: %d, f: %d, l: %d", *(int*)item, l - f, f, l);
+	ReleaseMutex(mutex_insert_lock);
+	sprintf_s(event, 65, "Buffer: %d inserted, l: %d", *(int*)item, l);
 	logger->log_event(event);
 	free(event);
 }
 
+/**
+ * Removes an element from the buffer. If the buffer is empty, this call will
+ * block until the remove can be performed. 
+ *
+ * @return A void pointer to the element removed from the buffer
+*/
 void *BC_Buffer::remove()
 {
 	WaitForSingleObject(sem_unavailable, INFINITE);
@@ -66,19 +103,26 @@ void *BC_Buffer::remove()
 	return item;
 }
 
+/**
+ * For internal class use only. Removes an element from the buffer. If this 
+ * method has been called, the buffer is guaranteed to be in a not empty 
+ * state. This call will block if the shared data of the buffer is currently
+ * in use.  
+ *
+ * @return A void pointer to the element removed from the buffer
+*/
 void *BC_Buffer::remove_internal()
 {
-	int f, l;
+	int f;
 	void *item;
 	char *event = (char*) calloc(65, sizeof(char));
-	WaitForSingleObject(mutex_lock, INFINITE);
+	WaitForSingleObject(mutex_remove_lock, INFINITE);
 	item = buffer[first % size];
 	buffer[first % size] = NULL;
 	first++;
 	f = first;
-	l = last;
-	ReleaseMutex(mutex_lock);
-	sprintf_s(event, 65, "Buffer: %d removed, l - f: %d, f: %d, l: %d", *(int*)item, l - f, f, l);
+	ReleaseMutex(mutex_remove_lock);
+	sprintf_s(event, 65, "Buffer: %d removed, f: %d", *(int*)item, f);
 	logger->log_event(event);
 	free(event);
 
--- BC_Buffer/BC_Buffer.hpp
+++ BC_Buffer/BC_Buffer.hpp
@@ -1,10 +1,13 @@
-/* File: BC_Buffer.h
-   Author: Brett Crawford
-   Date: 2014-
-   Prof: Kwatny
-   TAs: Liang and Casey
-   Course: CIS 3207, Sec 2
-   Description:
+/**
+ * @file BC_Buffer.hpp
+ * @author Brett Crawford
+ * @brief A shared, bounded buffer class header
+ * @details
+ *  Course: CIS 3207, Sec 2
+ *  Prof: Kwatny
+ *  TAs: Liang and Casey
+ *  Date: 2014-10-18
+ *  Description: The header for the buffer class.
 */
 
 #ifndef BC_BUFFER_H_
@@ -15,17 +18,23 @@
 
 #include "../BC_Logger/BC_Logger.hpp"
 
+/**
+ * A shared, bounded buffer class to be used with an implementation of the 
+ * producer/consumer problem. The buffer class is to be used in conjunction 
+ * with a shared logger as well as with producers and consumers.
+*/ 
 class BC_Buffer
 {
 	private:
-		size_t first;
-		size_t last;
-		size_t size;
-		void **buffer;
-		BC_Logger *logger;
-		HANDLE mutex_lock;            /* Mutual exclusion lock */
-		HANDLE sem_available;         /* Number of available slots in buffer */
-		HANDLE sem_unavailable;       /* Number of unavailable slots in buffer */
+		size_t first;               /**< The index of the first filled slot */
+		size_t last;                /**< The index of the first open slot */
+		size_t size;                /**< The max number of elements */
+		void **buffer;              /**< An array of void pointers */
+		BC_Logger *logger;          /**< A pointer to the shared logger */
+		HANDLE mutex_insert_lock;   /**< Mutex lock for insertions */
+		HANDLE mutex_remove_lock;   /**< Mutex lock for removals */
+		HANDLE sem_available;       /**< Number of available slots in buffer */
+		HANDLE sem_unavailable;     /**< Number of unavailable slots in buffer */
 
 		void insert_internal(void*);
 		void *remove_internal();
